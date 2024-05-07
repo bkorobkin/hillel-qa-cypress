71a5a4b46a617a266a177a5cc5d04b6835e05fe0
bkorobkin <korobkinbohdan@gmail.com>
2024-05-07 17:51:38 +0200

    config

--- cypress.config.js
+++ cypress.config.js
@@ -3,17 +3,22 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   // viewportHeight: 900,
   // viewportHeight: 1300,
-
+  defaultCommandTimeout: 10000,
+  watchForFileChanges: false,
     "e2e": {
       "setupNodeEvents": (on, config) => {
         
       },
-      "baseUrl": "https://qauto2.forstudy.space/",
+      "baseUrl": "https://guest:welcome2qauto@qauto2.forstudy.space",
       "isolation": false,
       "experimentalStudio": true,
-      "env": {
-        "username": process.env.LOGIN,
-        "password": process.env.PASSWORD
+      // "env": {
+      //   "username": process.env.LOGIN,
+      //   "password": process.env.PASSWORD
+      // }
+      env: {
+        "username": 'guest',
+        "password": 'welcome2qauto'
       }
     }
   })
